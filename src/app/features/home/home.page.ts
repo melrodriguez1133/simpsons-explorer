ebde2c2325b5bb8f9456896c006caa1dd18a59c8
melrodriguez1133 <202000231@est.umss.est>
2026-07-13 09:42:11 -0400

    feat: update home page to display characters, locations, and episodes with API integration

--- src/app/features/home/home.page.ts
+++ src/app/features/home/home.page.ts
@@ -7,11 +7,25 @@ import {
   IonTitle,
   IonToolbar,
   IonButtons,
-  IonMenuButton
+  IonMenuButton,
+  IonGrid,
+  IonRow,
+  IonCol
 } from '@ionic/angular/standalone';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 import { CardComponent } from 'src/app/shared/components/card/card.component';
- @Component({
+import {CharacterService} from 'src/app/core/services/character-service';
+import { LocationService } from 'src/app/core/services/location-service';
+import {EpisodeService} from 'src/app/core/services/episode-service';
+import { Location } from 'src/app/core/models/Location';
+import { LocationResponse } from 'src/app/core/interfaces/location-response.interface';
+import { IMAGE_CONSTANTS } from 'src/app/core/constants/image.constants';
+import { ImageSize } from 'src/app/core/enums/image-size.enum';
+import { Character } from 'src/app/core/models/Character';  
+import { CharacterResponse } from 'src/app/core/interfaces/character-response.interface';
+import { Episode } from 'src/app/core/models/Episode';
+import { EpisodeResponse } from 'src/app/core/interfaces/episode-response.interface';
+  @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
@@ -25,15 +39,40 @@ imports: [
   IonMenuButton,
   CommonModule,
   FormsModule,
+  IonGrid,
+  IonRow,
+  IonCol,
   HeaderComponent,
   CardComponent
 ]
 })
 export class HomePage implements OnInit {
-
-  constructor() { }
+  Locations: Location[] = [];
+  Characters: Character[] = []
+  Episodes: Episode[] = [];
+  protected readonly IMAGE_CONSTANTS = IMAGE_CONSTANTS
+  protected readonly ImageSize = ImageSize;
+  constructor(
+    private locationService: LocationService,
+    private characterService: CharacterService,
+    private episodeService: EpisodeService
+  ) { }
 
   ngOnInit() {
+this.locationService.getAllLocations().subscribe((response: LocationResponse) => {
+  this.Locations = response.results.slice(0, 5);
+  console.log(this.Locations);
+});
+
+this.characterService.getAllCharacters().subscribe((response: CharacterResponse) => {
+  this.Characters = response.results.slice(0, 5);
+  console.log(this.Characters);
+});
+
+this.episodeService.getAllEpisodes().subscribe((response: EpisodeResponse) => {
+  this.Episodes = response.results.slice(0, 5);
+  console.log(this.Episodes);
+});
   }
 
 }
